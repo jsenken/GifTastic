824addedf77c66e3e85ed819322d6cb315828bf2
Jordan Senken <jsenken@yahoo.com>
2018-11-18 01:10:41 -0500

    rebrand to basketball cards

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,8 @@ function renderButtons() {
     console.log("this worked"+ players);
     for (var i = 0; i < players.length; i++) {
           var a = $("<button>");
-          a.addClass("player");
+          a.addClass("btn btn-dark player");
+          a.attr("type","button")
           a.attr("data-name", players[i]);
           console.log(a.attr("data-name"));
           a.text(players[i]);
@@ -25,7 +26,7 @@ $("#addPlayer").on("click", function(event) {
         $("#players").empty();
         var player = $(this).attr("data-name");
         console.log("button clicked:" + player);
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=dc6zaTOxFJmzC&limit=4";
     
         $.ajax({
             url: queryURL,
@@ -35,15 +36,34 @@ $("#addPlayer").on("click", function(event) {
         .then(function(response) {
                 var results = response.data;
                 for (var i = 0; i < results.length; i++) {
-                  var gifDiv = $("<div class='item'>");
-                  var rating = results[i].rating;
-                  var p = $("<p>").text("Rating: " + rating);
-                  var playerImage = $("<img>");
-                  playerImage.attr("src", results[i].images.fixed_height.url);
-      
-                  gifDiv.prepend(p);
-                  gifDiv.prepend(playerImage);
-      
+                    var gifDiv = $("<div class='col-md-3'> ");
+                //   var rating = results[i].rating;
+                //   var p = $("<p>").text("Rating: " + rating);
+                    var back = $("<div class= 'background'>")
+                    var row1 = $("<div class='row'><div class='col-md-1'></div>");
+                    var row1b = $("<div class='col-md-10 gif'>");
+                    var row2 = $("<div class='row'><div class='col-md-1'></div><div class='col-md-3 logo'><img src='./assets/media/bball.png' class='basketball'></div><div class='name'>" + player + "</div> </div> ");
+   
+                    var playerImg = $("<img>");
+                    playerImg.attr("src", results[i].images.fixed_height.url);
+                    playerImg.addClass("playerImg");
+                    row1b.append(playerImg);
+                    row1.append(row1b);
+                    back.append(row1);
+                    back.append(row2);
+                    gifDiv.append(back);
+
+                    // gifDiv.append(row2);
+
+
+                    // gifDiv.prepend(c1);
+                    // gifDiv.prepend(gif);
+                    // gifDiv.prepend(c1);
+                    // gifDiv.prepend(row);
+                    // gifDiv.prepend(c1);
+                    // gifDiv.prepend(logo);
+                    // gifDiv.prepend(c6);
+                    // gifDiv.prepend(name);      
                   $("#players").prepend(gifDiv);
                 }
               });
